d72bcd224092488874c58a94a3859cb3cf396b94
unromanticman <umworksite@gmail.com>
2016-05-04 18:20:32 +0800

    add SQL test

---
+++ JAVAConnDB/test01/AccessJavaConn/src/AccessJavaConn.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class AccessJavaConn {
+    
+	/**
+	* 20160504 By UM
+	**/
+    
+	public static void main(String[] args) throws SQLException  {
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection("jdbc:ucanaccess:///Users/um/Desktop/pccudb/DB20160503/COMPANY.accdb");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		String stmtl = "select lname,salary from employee where ssn = ?" ;
+		PreparedStatement p = conn.prepareStatement(stmtl);
+		p.clearParameters();
+		
+		p.setString(1,"888665555");
+		ResultSet r = p.executeQuery();
+		
+		while (r.next()){
+			String lname = r.getString(1);
+			Double salary = r.getDouble(2);
+			System.out.println(lname + salary);
+		}
+		
+	}
+
+}
---
+++ JAVAConnDB/test01/MySQLJavaConn/src/MySQLJavaConn.java
@@ -0,0 +1,50 @@
+import java.sql.*;
+
+public class MySQLJavaConn {
+
+    /**
+     * 20160504 By UM
+     **/
+    
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		String driver = "com.mysql.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/company";
+		String user = "root";
+		String password = "";
+	
+		Connection conn = null;
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url,user,password);
+				if (conn != null && !conn.isClosed()) {
+			
+				}
+				else {
+				System.out.println("fail");
+				}
+		}
+		catch (ClassNotFoundException e) {
+			System.out.println("can not found driver class");
+			e.printStackTrace();
+			}
+			catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		String stmtl = "select lname,salary from employee where ssn = ?" ;
+		PreparedStatement p = conn.prepareStatement(stmtl);
+		p.clearParameters();
+
+		p.setString(1,"888665555");
+		ResultSet r = p.executeQuery();
+		
+		while (r.next()){
+			String lname = r.getString(1);
+			Double salary = r.getDouble(2);
+			System.out.println(lname + salary);
+		}
+			conn.close();
+		}
+	
+}
